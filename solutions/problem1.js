6a92dff7cbec1cc617fceabf9b8ae22caee04dc7
Steve Smith <steve@kentsmiths.com>
2019-01-27 12:38:31 -0500

    Got jasmine working with 1 placeholder test

--- solutions/problem1.js
+++ solutions/problem1.js
@@ -1,5 +1,12 @@
 'use strict';
 
+function Problem1() {}
+Problem1.prototype.solve = function() {
+    return sumMultiples(1000);
+};
+
+module.exports = Problem1;
+
 var sumMultiples = function(max) {
     var sum = 0;
     // TODO: find the sum of all multiples of 3 and 5 less than max
---
+++ spec/solutions/problem1Spec.js
@@ -0,0 +1,14 @@
+describe("problem1", function() {
+    var Problem1 = require('../../solutions/problem1');
+    var problem1;
+
+    beforeEach(function() {
+        problem1 = new Problem1();
+    });
+
+    it("should return correct answer from solve", function() {
+        let result = problem1.solve();
+
+        expect(result).toEqual(-1); // TODO: Change this to correct answer
+    });
+});
